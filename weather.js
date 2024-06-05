e3838f2a6dd3deff7416263e969474dc7f8cb6ca
scott <scottysora@gmail.com>
2024-06-05 15:25:52 -0400

    weather api app working

--- weather.js
+++ weather.js
@@ -1,20 +1,72 @@
 let cities = [
-    { name: "New York", lat: 40.7128, lon: -74.006 },
-    { name: "Los Angeles", lat: 34.0522, lon: -118.2437 },
-    { name: "Chicago", lat: 41.8781, lon: -87.6298 },
-    { name: "Houston", lat: 29.7633, lon: -95.3632 },
-    { name: "Phoenix", lat: 33.4484, lon: -112.0739 },
-    { name: "Philadelphia", lat: 39.9523, lon: -75.1631 },
-    { name: "San Antonio", lat: 29.4241, lon: -98.4936 },
-    { name: "San Diego", lat: 32.7157, lon: -117.1611 },
-    { name: "Dallas", lat: 32.7767, lon: -96.7969 },
-    { name: "San Jose", lat: 37.3382, lon: -121.8863 },
-    { name: "Austin", lat: 30.2671, lon: -97.7437 },
-    { name: "Jacksonville", lat: 30.3321, lon: -81.6557 },
-    { name: "San Francisco", lat: 37.7749, lon: -122.4194 },
-    { name: "Indianapolis", lat: 39.7684, lon: -86.158 },
-    { name: "Columbus", lat: 39.9611, lon: -82.9988 },
-    { name: "Fort Worth", lat: 32.7555, lon: -97.3308 },
-    { name: "Charlotte", lat: 35.2269, lon: -80.8431 },
-  ];
-  
+  { name: "New York", lat: 40.7128, lon: -74.006 },
+  { name: "Los Angeles", lat: 34.0522, lon: -118.2437 },
+  { name: "Chicago", lat: 41.8781, lon: -87.6298 },
+  { name: "Houston", lat: 29.7633, lon: -95.3632 },
+  { name: "Phoenix", lat: 33.4484, lon: -112.0739 },
+  { name: "Philadelphia", lat: 39.9523, lon: -75.1631 },
+  { name: "San Antonio", lat: 29.4241, lon: -98.4936 },
+  { name: "San Diego", lat: 32.7157, lon: -117.1611 },
+  { name: "Dallas", lat: 32.7767, lon: -96.7969 },
+  { name: "San Jose", lat: 37.3382, lon: -121.8863 },
+  { name: "Austin", lat: 30.2671, lon: -97.7437 },
+  { name: "Jacksonville", lat: 30.3321, lon: -81.6557 },
+  { name: "San Francisco", lat: 37.7749, lon: -122.4194 },
+  { name: "Indianapolis", lat: 39.7684, lon: -86.158 },
+  { name: "Columbus", lat: 39.9611, lon: -82.9988 },
+  { name: "Fort Worth", lat: 32.7555, lon: -97.3308 },
+  { name: "Charlotte", lat: 35.2269, lon: -80.8431 },
+];
+
+const cityList = document.querySelector("#city-dropdown-option");
+const weatherTable = document.querySelector("#weatherTableBody");
+
+function loadCityDropDownList() {
+  let defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.innerText = "Select City";
+  cityList.appendChild(defaultOption);
+
+  for (const city of cities) {
+    let option = document.createElement("option");
+    option.value = city.name;
+    option.innerText = city.name;
+    cityList.appendChild(option);
+  }
+}
+loadCityDropDownList();
+
+function getLocationPoint(city) {
+    fetch(`https://api.weather.gov/points/${city.lat},${city.lon}`)
+     .then(response => response.json())
+     .then(point => fetch(point.properties.forecast))
+     .then(response => response.json())
+     .then(forecast => {
+        weatherTable.innerText = "";
+        forecast.properties.periods.forEach(buildTableRow);
+      });
+  }
+
+function buildTableRow(period) {
+  let row = weatherTable.insertRow();
+
+  let tableData1 = row.insertCell();
+  tableData1.innerText = period.name;
+
+  let tableData2 = row.insertCell();
+  tableData2.innerText = `Temperature: ${period.temperature} F`;
+
+  let tableData3 = row.insertCell();
+  tableData3.innerText = period.detailedForecast;
+}
+
+cityList.addEventListener("change", handleCityChanged);
+
+function handleCityChanged(event) {
+  const cityName = event.target.value;
+
+  const selectedCity = cities.find(function (city) {
+    return city.name == cityName;
+  });
+  getLocationPoint(selectedCity);
+}
